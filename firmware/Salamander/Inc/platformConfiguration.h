7906df4d56a026d3b5736fbfe0b5adc518b9d7a3
Alex Kempster <alkempster.cu@gmail.com>
2016-09-14 17:23:41 +0100

    started adding driver for LEDs

---
+++ firmware/Salamander/Inc/platformConfiguration.h
@@ -0,0 +1,17 @@
+/*!
+ * \file	platformConfiguration.h
+ *
+ * \date	Sep 14, 2016
+ * \author	Alex
+ *
+ * \brief
+ * This file is used to configure the system based on the processor and
+ * attached hardware available
+ */
+
+#ifndef INC_PLATFORMCONFIGURATION_H_
+#define INC_PLATFORMCONFIGURATION_H_
+
+#define STM32F429NUCLEO
+
+#endif /* INC_PLATFORMCONFIGURATION_H_ */
---
+++ firmware/Salamander/Src/LED.c
@@ -0,0 +1,61 @@
+/*!
+ * \file	LED.c
+ *
+ * \date	Sep 14, 2016
+ * \author	Alex
+ *
+ * \brief
+ */
+
+/* Library include files */
+#include <stdbool.h>
+
+/* Project include files */
+#include "LED.h"
+
+/* Interface functions */
+
+/*!
+ * \brief
+ * Initialise a GPIO line to be used as an LED output
+ *
+ * \param port	the port which the pin(s) reside
+ * \param pins	the pin(s) to set for LED's, if they are on the same port
+ * 				then the pins can be configured simultaneously.
+ */
+void LED_Init(GPIO_TypeDef* port, uint16_t pins)
+{
+	GPIO_InitTypeDef init;
+
+	init.Mode = GPIO_MODE_OUTPUT_PP;
+	init.Pin = pins;
+	init.Pull = GPIO_NOPULL;
+	init.Speed = GPIO_SPEED_FREQ_LOW;
+
+	HAL_GPIO_Init(port, &init);
+
+}
+
+/*!
+ * \brief
+ * turns an LED on or off. use the LED_drive to determine if the LED should be
+ * on or off. The HIGH LOW part of the enum indicates if the LED is active
+ * high or active low.
+ *
+ * \param port	the port which the pin resides
+ * \param pin	the pin to turn on/off
+ * \param state	which state to turn the LED and how the LED is driven
+ */
+void LED_Set(GPIO_TypeDef* port, uint16_t pin, LED_drive state)
+{
+	// if need to drive an output logic 1
+	if ((state == LED_ON_HIGH) || (state == LED_OFF_HIGH))
+	{
+
+	} else	// output logic 0
+	{
+
+	}
+}
+
+/* Module functions */
